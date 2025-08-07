cf2711aa6e73ca3506d4dd64881c2dc91eb21dce
Calum Price <calumaprice@gmail.com>
2025-08-07 18:58:10 +0100

    added hyprpaper to hyprecosystem script

--- install/deskenv/hyprecosystem.sh
+++ install/deskenv/hyprecosystem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 paru -S --noconfirm \
-    hyprland hyprshot hyprlock hypridle hyprland-qtutils hyprsysteminfo hyprpolkitagent hyprlang \
+    hyprland hyprshot hyprlock hypridle hyprland-qtutils hyprsysteminfo hyprpolkitagent hyprlang hyprpaper \
     rofi waybar swaync \
     xdg-desktop-portal-hyprland
 
