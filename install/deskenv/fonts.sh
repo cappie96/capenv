d928a07dcddb7466ed83888d4d17063d0b1ff067
Calum Price <calumaprice@gmail.com>
2025-11-23 10:52:43 +0000

    removed unwanted font imwriting

--- install/deskenv/fonts.sh
+++ install/deskenv/fonts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd ttf-iawriter-nerd
+paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd 
